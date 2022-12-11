4867ffa3a97c2f1238650b76db8cc17e4f45ff1d
QLing-yes <z3182257779@outlook.com>
2022-12-11 21:38:48 +0800

    优化深响应逻辑

--- src/tools/tools.ts
+++ src/tools/tools.ts
@@ -1,3 +1,4 @@
+
 /** 类型判断 */
 export function isType(obj: any) {
     if (obj !== obj) return 'NaN';
@@ -8,40 +9,41 @@ export function isType(obj: any) {
  * @param callback 响应回调
  * @param deep 深响应(默认false)
  */
-export function ref<T extends object>(obj: T, callback: (v: T) => void, deep = false) {
+export function ref<T extends object, K extends keyof T>(obj: T, callback: (o: T, k: K) => void, deep = false) {
     if (deep) {
+        function subProxy(o: T, k: K) {
+            const l = Reflect.get(o, k);
+            if (l instanceof Object && Object.getPrototypeOf(l).isProxy != Proxy) {
+                Reflect.set(o, k, new Proxy(l as object, Handler));
+            }
+        }
         const Handler: ProxyHandler<T> = {
             getPrototypeOf(proto) {
-                return { proto, isProxy: true }
+                Object.getPrototypeOf(proto).isProxy = Proxy;
+                return proto;
             },
-            get: (o, k) => {
-                subProxy(o, k)
-                return Reflect.get(o, k)
+            get(o, k) {
+                subProxy(o, k as K);
+                return Reflect.get(o, k);
             },
             set(o, k, v) {
-                Reflect.set(o, k, v)
-                subProxy(o, k)
-                callback(o)
+                Reflect.set(o, k, v);
+                subProxy(o, k as K);
+                callback(o, k as K);
                 return true;
             }
         }
-        function subProxy<T extends object>(o: T, k: string | symbol) {
-            let l = Reflect.get(o, k)
-            if ((typeof l) == 'object' && Object.getPrototypeOf(l).isProxy != true) {
-                Reflect.set(o, k, new Proxy(l, Handler))
-            }
-        }
-        return new Proxy<T>(obj, Handler)
+        return new Proxy<T>(obj, Handler);
     } else {
-        type k = keyof T;
         return new Proxy(obj, {
             getPrototypeOf(proto) {
-                return { proto, isProxy: true }
+                Object.getPrototypeOf(proto).isProxy = Proxy;
+                return proto;
             },
             get: (o, k) => Reflect.get(o, k),
             set(o, k, v) {
-                Reflect.set(o, k, v)
-                callback(obj)
+                Reflect.set(o, k, v);
+                callback(o, k as K);
                 return true;
             }
         })
